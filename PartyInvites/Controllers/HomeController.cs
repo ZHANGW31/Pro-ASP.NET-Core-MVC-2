475e509f07c1c81e4e35df472dd353d1aded41ac
Wei Zhang <wei.zhang4@outlook.com>
2020-02-24 18:05:03 -0800

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller //Controllers will always have Controller as the suffix, in this case this Controller is called Home.
+    {
+        public string Index() //This method displays hello world when the web application is ran.
+        {
+            return "Hello World"; 
+        }
+    }
+}
